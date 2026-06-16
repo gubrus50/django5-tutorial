16647df97c40d47f7ed8417b4be2dd0b67977fff
McRaZick <mcrazick@mail.com>
2026-06-17 00:16:59 +0100

    Redirect Django Admin Login page

--- project_folder/my_website/urls.py
+++ project_folder/my_website/urls.py
@@ -15,11 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, reverse
+from django.shortcuts import redirect
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+
+def admin_login_redirect(request):
+    return redirect(f'{reverse('login')}?next=/admin/')
+
+
 urlpatterns = [
+    path('admin/login/', admin_login_redirect),
     path('admin/', admin.site.urls),
     path('', include('app_name.urls')),
     path('users/', include('users.urls')),
